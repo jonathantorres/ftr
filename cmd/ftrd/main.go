8dad4cb02fed6439d1d494446e0a215ce2034577
Jonathan Torres <jonathantorres41@gmail.com>
2022-03-27 01:49:05 -0400

    Add several comments and default usage message

--- cmd/ftrd/main.go
+++ cmd/ftrd/main.go
@@ -10,12 +10,27 @@ import (
 	"github.com/jonathantorres/ftr/internal/server"
 )
 
+// TODO: look into adding a "prefix" value that will specify
+// the prefix of the server (a location in the filesystem ex. /usr/local/ftr)
+// this location will contain the configuration files and all the log files
+// will be stored here, this value should be set at build time,
+// the default value will be /usr/local/ftr, this value can be overriden
+// by using the command line flag -p (prefix)
+
 var hostFlag = flag.String("host", server.DefaultHost, "The host of the server")
 var portFlag = flag.Int("port", server.ControlPort, "The port number for the control connection")
 var confFlag = flag.String("conf", server.DefaultConf, "The location of the configuration file")
 
 func main() {
+	flag.Usage = usage
 	flag.Parse()
+
+	// TODO: start logging mechanism
+
+	// TODO: install signals to manage the server
+	//       SIGINT, SIGTERM and SIGQUIT to shutdown the server
+	//       SIGHUB to reload the configuration file
+
 	conf, err := loadConf(*confFlag)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "server conf error: %s\n", err)
@@ -44,4 +59,9 @@ func loadConf(configLoc string) (*server.ServerConf, error) {
 		return nil, err
 	}
 	return conf, nil
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: ftr -[hv] [-p port] [-h host] [-c conf]\n")
+	flag.PrintDefaults()
 }
