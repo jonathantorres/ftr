831c5a868fb37100fc2f8949df2c49c9efd41c88
Jonathan Torres <jonathantorres41@gmail.com>
2022-07-03 11:30:02 -0400

    Fixes when reading the command from the client and parsing it

--- src/session.cpp
+++ src/session.cpp
@@ -13,11 +13,9 @@ using namespace ftr;
 
 void Session::start() {
     while (true) {
-        std::array<char, ftr::DEFAULT_CMD_SIZE> client_cmd;
+        std::array<char, ftr::DEFAULT_CMD_SIZE> client_cmd = {0};
 
-        int res =
-            read(control_conn_fd, reinterpret_cast<char *>(client_cmd.data()),
-                 client_cmd.size());
+        int res = read(control_conn_fd, client_cmd.data(), client_cmd.size());
 
         if (res < 0) {
             // an error ocurred
@@ -85,6 +83,7 @@ void Session::handle_command(
 
     if (cmd_params == "") {
         exec_command(cmd, "");
+        return;
     }
 
     exec_command(cmd, cmd_params);
@@ -92,72 +91,103 @@ void Session::handle_command(
 
 void Session::exec_command(std::string cmd, std::string cmd_params) {
     // TODO: log the command executed
-    std::cerr << cmd << ' ' << cmd_params;
 
     if (cmd == CMD_USER) {
         run_user();
+        return;
     } else if (cmd == CMD_PASSWORD) {
         run_password();
+        return;
     } else if (cmd == CMD_PRINT_DIR) {
         run_print_dir();
+        return;
     } else if (cmd == CMD_CHANGE_DIR) {
         run_change_dir();
+        return;
     } else if (cmd == CMD_TYPE) {
         run_type();
+        return;
     } else if (cmd == CMD_PASSIVE) {
         run_passive();
+        return;
     } else if (cmd == CMD_LIST) {
         run_list();
+        return;
     } else if (cmd == CMD_FILE_NAMES) {
         run_file_names();
+        return;
     } else if (cmd == CMD_RETRIEVE) {
         run_retrieve();
+        return;
     } else if (cmd == CMD_ACCEPT_AND_STORE || cmd == CMD_STORE_FILE) {
         run_accept_and_store();
+        return;
     } else if (cmd == CMD_APPEND) {
         run_accept_and_store(); // uses a different flag
+        return;
     } else if (cmd == CMD_SYSTEM_TYPE) {
         run_system_type();
+        return;
     } else if (cmd == CMD_CHANGE_PARENT || cmd == CMD_CHANGE_TO_PARENT_DIR) {
         run_change_parent();
+        return;
     } else if (cmd == CMD_MAKE_DIR || cmd == CMD_MAKE_A_DIR) {
         run_make_dir();
+        return;
     } else if (cmd == CMD_REMOVE_DIR) {
         run_remove_dir();
+        return;
     } else if (cmd == CMD_DELETE) {
         run_delete();
+        return;
     } else if (cmd == CMD_EXT_PASSV_MODE) {
         run_ext_passv_mode();
+        return;
     } else if (cmd == CMD_PORT) {
         run_port();
+        return;
     } else if (cmd == CMD_EXT_ADDR_PORT) {
         run_ext_addr_port();
+        return;
     } else if (cmd == CMD_HELP) {
         run_help();
+        return;
     } else if (cmd == CMD_NOOP) {
         run_noop();
+        return;
     } else if (cmd == CMD_ALLO) {
         run_allo();
+        return;
     } else if (cmd == CMD_ACCOUNT) {
         run_account();
+        return;
     } else if (cmd == CMD_SITE) {
         run_site();
+        return;
     } else if (cmd == CMD_MODE) {
         run_mode();
+        return;
     } else if (cmd == CMD_ABORT) {
         run_abort();
+        return;
     } else if (cmd == CMD_FILE_STRUCT) {
         run_file_struct();
+        return;
     } else if (cmd == CMD_SERVER_STATUS) {
         run_server_status();
+        return;
     } else if (cmd == CMD_RENAME_FROM) {
         run_rename_from();
+        return;
     } else if (cmd == CMD_RENAME_TO) {
         run_rename_to();
+        return;
     } else if (cmd == CMD_REINIT) {
         run_reinit();
+        return;
     } else if (cmd == CMD_QUIT) {
         run_quit();
+        return;
     }
 
     run_not_implemented();
