be0f6984ecf70b3adf83f227d471401b62e0e220
Jonathan Torres <jonathantorres41@gmail.com>
2022-07-31 17:41:27 -0400

    Remove unneeded code that deals with strings since now it's included in the strings library, also cleanup tests

--- src/util.hpp
+++ src/util.hpp
@@ -2,18 +2,9 @@
 #define ftr_util_hpp
 
 #include <string>
-#include <vector>
 
 namespace ftr {
 
-std::string trim_whitespace(const std::string &s);
-std::string trim_right(const std::string &s);
-std::string trim_left(const std::string &s);
-std::string to_lower(const std::string &s);
-std::string to_upper(const std::string &s);
-std::vector<std::string> split(const std::string &s, const std::string &delim);
-std::string join(const std::vector<std::string> &contents,
-                 const std::string &delim);
 bool is_ipv4(const std::string &s);
 bool is_ipv6(const std::string &s);
 bool is_domain_name(const std::string &s);
