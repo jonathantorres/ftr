fed9dbe24229f985d578992934b450eee91b97bb
Jonathan Torres <jonathantorres41@gmail.com>
2023-02-17 20:26:23 -0500

    Improve the information displayed for files when doing the LIST command

--- internal/server/file.go
+++ internal/server/file.go
@@ -3,11 +3,72 @@ package server
 import (
 	"fmt"
 	"os"
+	"os/user"
+	"strconv"
+	"strings"
+	"syscall"
 )
 
+const defaultDirSize = 4096
+
 func getFileLine(file os.FileInfo) string {
-	mode := file.Mode().String()
-	size := file.Size()
-	modTime := fmt.Sprintf("%s %d %d", file.ModTime().Month().String(), file.ModTime().Day(), file.ModTime().Year())
-	return fmt.Sprintf("%s %d %s %s", mode, size, modTime, file.Name())
+	var line strings.Builder
+
+	// file mode bits
+	line.WriteString(file.Mode().String())
+
+	// owner and group name
+	line.WriteString(" " + getOwnerName(file))
+	line.WriteString(" " + getGroupName(file))
+
+	// file size
+	if file.IsDir() {
+		line.WriteString(fmt.Sprintf(" %d", defaultDirSize))
+	} else {
+		line.WriteString(fmt.Sprintf(" %d", file.Size()))
+	}
+
+	// modification time
+	line.WriteString(" " + file.ModTime().Format("Jan 01 15:05"))
+
+	// file name
+	line.WriteString(" " + file.Name())
+
+	return line.String()
+}
+
+func getGroupName(file os.FileInfo) string {
+	groupName := "unknown"
+	stat, ok := file.Sys().(*syscall.Stat_t)
+
+	if ok {
+		gId := strconv.FormatUint(uint64(stat.Gid), 10)
+		group, err := user.LookupGroupId(gId)
+
+		if err != nil {
+			return groupName
+		}
+
+		groupName = group.Name
+	}
+
+	return groupName
+}
+
+func getOwnerName(file os.FileInfo) string {
+	ownerName := "unknown"
+	stat, ok := file.Sys().(*syscall.Stat_t)
+
+	if ok {
+		uId := strconv.FormatUint(uint64(stat.Uid), 10)
+		usr, err := user.LookupId(uId)
+
+		if err != nil {
+			return ownerName
+		}
+
+		ownerName = usr.Username
+	}
+
+	return ownerName
 }
